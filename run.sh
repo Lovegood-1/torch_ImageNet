00a8e5620a640b1475c4eb4bd1f36fa00815f002
niejianzheng <niejianzheng@dm-ai.cn>
2019-09-30 17:11:44 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 python -m torch.distributed.launch --nproc_per_node=8 imagenet.py \
-                                    --a resnet18 --b 256  \
                                     --pretrained --lr=0.01  /data/behavior_data/student_data/lexue
